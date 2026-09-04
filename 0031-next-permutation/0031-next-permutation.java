b979d71b145d660f9a1ebf767b8dc724e143e634
Kumari priyanka <priyanka190807@gmail.com>
2026-09-04 22:58:08 +0530

    Time: 0 ms (100%), Space: 44.8 MB (33.36%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,45 +1,35 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
+        int n=nums.length;
+        int gola_index=-1;
 
-        // Find the gola_index
-        int gola_index = -1;
-
-        for (int i = n - 1; i > 0; i--) {
-            if (nums[i] > nums[i - 1]) {
-                gola_index = i - 1;
+        for(int i=n-1;i>0;i--){
+            if(nums[i-1]<nums[i]){
+                gola_index=i-1;
                 break;
             }
         }
-
-        // If a gola_index was found
-        if (gola_index != -1) {
-            int swap_index = gola_index;
-
-            // Find the smallest value greater than nums[gola_index]
-            for (int j = n - 1; j > gola_index; j--) {
-                if (nums[j] > nums[gola_index]) {
-                    swap_index = j;
+        if(gola_index!=-1){
+            int swap_index=gola_index;
+            for(int j=n-1;j>gola_index;j--){
+                if(nums[j]>nums[gola_index]){
+                    swap_index=j;
                     break;
                 }
             }
 
-            // Swap
-            int temp = nums[gola_index];
-            nums[gola_index] = nums[swap_index];
-            nums[swap_index] = temp;
+            int temp=nums[gola_index];
+            nums[gola_index]=nums[swap_index];
+            nums[swap_index]=temp;
         }
-
-        // Reverse the suffix
-        reverse(nums, gola_index + 1, n - 1);
+        reverse(nums, gola_index+1, n-1);
+        
     }
-
-    private void reverse(int[] nums, int left, int right) {
-        while (left < right) {
-            int temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-
+    private void reverse(int[] nums, int left, int right){
+        while(left<right){
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
             left++;
             right--;
         }
