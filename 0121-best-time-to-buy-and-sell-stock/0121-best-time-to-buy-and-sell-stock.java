816d5495760967258715375436fd5aa9e29d529b
Kumari priyanka <priyanka190807@gmail.com>
2026-09-20 00:24:35 +0530

    Time: 2 ms (45.56%), Space: 94.6 MB (33.02%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,16 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int minprice=prices[0];
-        int maxprofit=0;
-        for(int i=1;i<prices.length;i++){
+        int maxprice=0;
+        for(int i=1;i<prices.length; i++){
+            int currprice=prices[i]-minprice;
+            maxprice=Math.max(currprice, maxprice);
             minprice=Math.min(minprice, prices[i]);
-            int currprofit= prices[i]-minprice;
-            maxprofit=Math.max(currprofit,maxprofit);
         }
-        return maxprofit;
+        return maxprice;
+        
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
